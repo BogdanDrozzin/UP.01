621835d77c47f9e592db8967367b6a0e6a2b40e8
BogdanDrozzin <komka838@gmail.com>
2025-01-22 16:08:23 +0300

    Delete DrozzinBogdan_task2_4 directory

--- DrozzinBogdan_task2_4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.drozzin.drozzinbogdan_task2_4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.drozzin.drozzinbogdan_task2_4 to javafx.fxml;
-    exports ru.drozzin.drozzinbogdan_task2_4;
-}
